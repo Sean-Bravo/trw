f1b3732723e64acf499ce210e4a3b520b47b3081
Sean-Bravo <Sean-Bavo@proton.me>
2026-01-22 20:38:17 -0500

    page.tsx update

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
       <main className="min-h-screen">
         <Header />
         <Hero />
-        <BankFeature />
         <HowItWorks />
         <SocialProof />
         <Integrations />
+        <BankFeature />
         <TrustEngine />
         <Pricing />
         <FAQ />
